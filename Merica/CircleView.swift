7ac0f9ca2b1b0358988cf0f30496c620bb50196a
Matt Deuschle <mdeuschle@gmail.com>
2017-08-01 18:47:42 -0700

    circle view

---
+++ Merica/CircleView.swift
@@ -0,0 +1,18 @@
+//
+//  CircleView.swift
+//  Merica
+//
+//  Created by Matt Deuschle on 8/1/17.
+//  Copyright © 2017 Matt Deuschle. All rights reserved.
+//
+
+import UIKit
+
+class CircleView: UIImageView {
+
+    override func layoutSubviews() {
+        layer.cornerRadius = self.frame.width / 2
+        clipsToBounds = true
+    }
+}
+
